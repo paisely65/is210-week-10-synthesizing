e206269673f3caf10a364551a90b63c532c3df02
paisely65 <cunysps@gmail.com>
2016-03-31 16:56:05 -0400

    Task 1 with loop.

--- task_01.py
+++ task_01.py
@@ -1,56 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A small docstring that correlates two tasks."""
-
-import data
-print data.CUSTOMERS
-CUSTOMERS = {
-1: {'name': 'Ekaterin Vorsoisson', 'email': 'evorsoisson@komarr.net'},
-2: {'name': 'Cordelia Naismith', 'email': 'cordelia@beta.com'},
-3: {'name': 'Ivan Vorpatril', 'email': 'iv398@barrayar.net'},
-4: {'name': 'Aral Vorkosigan', 'email': 'viceroy@sergyar.net'},
-5: {'name': 'Eli Quinn', 'email': 'admiral@dendarii.com'},
-}
-
-ORDERS = {
-27: {'customer_id': 1, 'total': 1},
-13: {'customer_id': 1, 'total': 287},
-36: {'customer_id': 1, 'total': 999},
-4: {'customer_id': 5, 'total': 18},
-6: {'customer_id': 4, 'total': 243},
-8: {'customer_id': 4, 'total': 536},
-15: {'customer_id': 2, 'total': 92},
-17: {'customer_id': 3, 'total': 148},
-19: {'customer_id': 2, 'total': 159},
-21: {'customer_id': 2, 'total': 984},
-32: {'customer_id': 5, 'total': 381},
-34: {'customer_id': 3, 'total': 183},
-35: {'customer_id': 2, 'total': 813},
-37: {'customer_id': 3, 'total': 27},
-38: {'customer_id': 5, 'total': 52},
-39: {'customer_id': 4, 'total': 30},
-40: {'customer_id': 2, 'total': 730},
-41: {'customer_id': 4, 'total': 28},
-42: {'customer_id': 4, 'total': 370},
-}
+"""A small docstring that summarizes sustomer orders."""
 
 
 def sum_orders(customers, orders):
-
-    
     """This formula finds the sum of customers orders.
 
     Args:
-        customers(dict): A dictionary of customer_id (required)
-        orders(dict): A dictionary of orders keyed by order id (required)
+        customers(dict): A dictionary of keyed by customer_id.
+        orders(dict): A dictionary of orders keyed by order id
 
     Returns:
-        dictionary: A nested dictionary
+        dictionary: A dictionary that combines customers and orders
 
     Examples:
-    """
- 
-        
+        >>>CUSTOMERS = 1: {'name': 'Ekaterin Vorsoisson', 'email': 'evorsoisson@komarr.net'},
+        2: {'name': 'Cordelia Naismith', 'email': 'cordelia@beta.com'},
+        3: {'name': 'Ivan Vorpatril', 'email': 'iv398@barrayar.net'},
 
-    
-    
+        >>> ORDERS = 13: {'customer_id': 1, 'total': 287},
+        15: {'customer_id': 2, 'total': 92},
+        17: {'customer_id': 3, 'total': 148},
+        19: {'customer_id': 2, 'total': 159},
+        21: {'customer_id': 2, 'total': 984},
+        27: {'customer_id': 1, 'total': 1},
+        34: {'customer_id': 3, 'total': 183},
+        35: {'customer_id': 2, 'total': 813},
+        36: {'customer_id': 1, 'total': 999},
+        37: {'customer_id': 3, 'total': 27},
+        40: {'customer_id': 2, 'total': 730},
+
+        >>> sum_orders(customers=CUSTOMERS), orders=ORDERS)
+    """
+
+    customer_database = {}
+    for key, value in customers.iteritems():
+        total_orders = 0
+        num_orders = 0
+        for request in orders.values():
+            if key == request['customer_id']:
+                total_orders += request['total']
+                num_orders += 1
+                customer_database = {'name': value['name'],
+                                     'email':value['email'],
+                                     'orders': num_orders,
+                                     'total':total_orders}
+    return customer_database
