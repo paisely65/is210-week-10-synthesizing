e325c30af6c1c0addcad3115053626133db93f03
paisely65 <cunysps@gmail.com>
2016-03-31 15:32:49 -0400

    Task 1 is finished.

---
+++ task_01.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A small docstring that correlates two tasks."""
+
+
+import data
+print data
+def sum_orders(customers, orders):
+    """This formula finds the sum of customers orders.
+
+    Args:
+        customers(dict): A dictionary of customer_id (required)
+        orders(dict): A dictionary of orders keyed by order id (required)
+
+    Returns:
+        dictionary: A nested dictionary
+
+    Examples:
+         >>>ODRERS = {1: {'customer_id': 11, 'total': 110},
+	              3: {'customer_id': 33, 'total': 330},
+	              5: {'customer_id': 55, 'total': 550}}
+        >>> CUSTOMERS = {11: {'name': 'Alice', 'email': 'alice@gmail.com'},
+	                 33: {'name': 'Cathy' 'email': 'Cathy@gmail.com'},
+
+    """
+    sum_orders(customers=CUSTOMERS, orders=ORDERS)
+    return 'sum_orders'
+
